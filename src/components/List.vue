e05083a55f1b1eb70db0087a95ca32ff45973acf
mariusz-dabal <mariusz9dabal@gmail.com>
2020-02-24 19:22:09 +0100

    Initial commit

---
+++ src/components/List.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list">
+      <h4>List</h4>
+      <ul>
+          <li :key="post.id" v-for="post in posts" v-text="post.name"></li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'List',
+
+  mounted: function () {
+      axios.get('http://jobagent.test/users')
+        .then(response => this.posts = response.data)
+  },
+
+  data() {
+      return {
+          posts: null
+      }
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
